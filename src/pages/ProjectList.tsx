c00c9b9a62f8e173635c3ef339ebc5c06d88276b
karan26796 <karan26796@gmail.com>
2024-10-13 00:43:31 +0530

    Added indie finds card

--- src/pages/ProjectList.tsx
+++ src/pages/ProjectList.tsx
@@ -3,6 +3,7 @@ import { projectSummaries } from '../utils/ProjectSummaries';
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import '../styles/ProjectList.scss';
 import { Link } from 'react-router-dom';
+import ProjectCardSmall from '../components/ProjectCardSmall';
 
 interface ProjectListProps {
   projectData: typeof projectSummaries; // Assuming projectSummaries is the type you want
@@ -12,6 +13,7 @@ interface ProjectListProps {
 const ProjectList: React.FC<ProjectListProps> = ({ projectData, cardComponent: CardComponent }) => {
   return (
     <div className='project-parent'>
+      <ProjectCardSmall/>
       {projectData.map((project) => (
         <Link key={project.id} to={`/project/${project.id}`} style={{ textDecoration: 'none' }}>
           <CardComponent data={project} />
