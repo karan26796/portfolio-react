0279c57f35c11ed64b2685772164acd05268d5dd
karan26796 <karan26796@gmail.com>
2024-10-22 15:11:43 +0530

    project header updated

--- src/components/ProjectHeader.tsx
+++ src/components/ProjectHeader.tsx
@@ -1,48 +1,38 @@
-// ProjectCard.tsx
-
 import React from "react";
 import { ProjectCardData } from "../utils/interfaces";
 import "../styles/ProjectHeader.scss";
+import Tag, { VibrantColor } from "./Tag";
+
+// Define vibrant colors array
+const vibrantColors: VibrantColor[] = [
+  { bg: '#fefefe', text: '#FF2D55' }, // Pink
+  { bg: '#fefefe', text: '#007AFF' }, // Blue
+  { bg: '#fefefe', text: '#34C759' }, // Green
+  { bg: '#fefefe', text: '#FF9500' }, // Orange
+  { bg: '#fefefe', text: '#AF52DE' }, // Purple
+  { bg: '#fefefe', text: '#00C7BE' }  // Teal
+];
 
 const ProjectHeader: React.FC<{ data: ProjectCardData }> = ({ data }) => {
   return (
     <div className="project-header">
       <div className="project-header-data">
         <h1>{data.title}</h1>
-        <h5>{data.tags.join(", ")}</h5>
+        <div className="tag-container">
+          {data.tags.map((tag, index) => (
+            <Tag
+              key={index}
+              text={tag}
+              color={vibrantColors[index % vibrantColors.length]}
+              rotation={0}
+              variant="small"
+            />
+          ))}
+        </div>
       </div>
       <img className="project-header-image" src={data.img} alt={data.title} />
-      {/* <div className="divider"></div> */}
     </div>
   );
 };
 
 export default ProjectHeader;
-
-{
-  /* <a
-  key={project.id}
-  className="project-card"
-  onClick={() => navigate(`/projects/${project.id}`)}
->
-  <div className="img-container">
-    <img src={project.imageUrl} alt={project.title} className="project-image" />
-  </div>
-  <div className="project-text">
-    <p style={{ fontSize: "1.15rem" }} className="semibold">
-      {project.title}
-    </p>
-  </div>
-  <div className="project-controls">
-    <div className="project-meta text">
-      <p style={{ fontSize: ".8rem" }} className="project-tags ">
-        {project.tags.join(", ")}
-      </p>
-    </div>
-    <button className="read-more text">
-      Read More
-      <i className="material-icons">arrow_forward</i>
-    </button>
-  </div>
-</a>; */
-}
